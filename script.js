040fa8c183b73c201815e48be1352c320be9eb8e
Nathaniel Giron <nathaniel.giron@gmail.com>
2016-03-30 09:27:19 -0700

    added sortable

--- script.js
+++ script.js
@@ -3,14 +3,17 @@ $('#myForm').submit(function(event){
 	var todo = $('input:text').val();
 
 	if (todo != '') {
-		$('ul').append("<li>"+todo+" <button id='del'>x</button> </li>");
+		$('ul').append("<li>"+todo+" <button id='del' class='ui-state-default'>x</button> </li>");
 		$('input:text').val('');
 		console.log(todo);
 	}
 });
 
-$(document).on('click', '#del', function() {
+$(document).on('click','#del', function() {
 	$(this).parent().remove();
 	console.log('clicked');
 });
 
+$(function() {
+    $( "#sortable" ).sortable();
+});
