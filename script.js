1a0b7b0f8971821be721db309ddc1e8bf5ab7bf3
Nathaniel Giron <nathaniel.giron@gmail.com>
2016-03-30 10:51:05 -0700

    added localstorage functionality

--- script.js
+++ script.js
@@ -1,19 +1,32 @@
+var todos = JSON.parse(localStorage.getItem('todos')) || [];
+
+var createToDo = function(text) {
+	var html = "<li>"+text+" <button id='del' class='ui-state-default'>x</button> </li>";
+	$('ul').append(html);
+	$('input:text').val('');
+}
+
+for(var i = 0; i < todos.length; i++) {
+	createToDo(todos[i]);
+}
+
 $('#myForm').submit(function(event){
 	event.preventDefault();
 	var todo = $('input:text').val();
 
 	if (todo != '') {
-		$('ul').append("<li>"+todo+" <button id='del' class='ui-state-default'>x</button> </li>");
-		$('input:text').val('');
-		console.log(todo);
+		todos.push(todo);
+		localStorage.setItem('todos', JSON.stringify(todos));
+
+		createToDo(todo);
 	}
 });
 
 $(document).on('click','#del', function() {
+	var todoIndex = todos.indexOf($(this).prev().text());
+	todos.splice(todoIndex,1);
+	localStorage.setItem('todos', JSON.stringify(todos));
 	$(this).parent().remove();
-	console.log('clicked');
 });
 
-$(function() {
-    $( "#sortable" ).sortable();
-});
+$( "#sortable" ).sortable();
